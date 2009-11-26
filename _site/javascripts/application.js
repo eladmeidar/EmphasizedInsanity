31ea8404fffbd48491eb43e1b624b56383490682
Elad Meidar <elad@eizesus.com>
2009-11-25 23:27:15 -0500

    Sinatra template

--- _site/javascripts/application.js
+++ _site/javascripts/application.js
@@ -1,21 +1,30 @@
-$(document).ready(function(){
-	initMenu();
-});
+//<!--
+  function toggle(id) {
+    var pre  = document.getElementById("pre-" + id);
+    var post = document.getElementById("post-" + id);
+    var context = document.getElementById("context-" + id);
 
-function initMenu(){
-	$('#menuTrigger').click(function(e) {
+    if (pre.style.display == 'block') {
+      pre.style.display = 'none';
+      post.style.display = 'none';
+      context.style.background = "none";
+    } else {
+      pre.style.display = 'block';
+      post.style.display = 'block';
+      context.style.background = "#fffed9";
+    }
+  }
 
-		e.preventDefault();
-		triggerEffect(this,'toolbarContainer','slide', '/images/rbnDownButton.png', '/images/rbnUpButton.png');
-	});
-}
+  function toggleBacktrace(){
+    var bt = document.getElementById("backtrace");
+    var toggler = document.getElementById("expando");
 
-function triggerEffect(srcElement,divId, effect, firstImg, secondImg){
-
-  $("#" + divId).slideToggle("normal", function(){
-	image = srcElement.getElementsByTagName('img')[0];
-	var new_image = $(image).attr("src").match(firstImg) ? secondImg : firstImg;
-    $(image).attr("src", new_image); 
-  });
-  return false;
-}
+    if (bt.className == 'condensed') {
+      bt.className = 'expanded';
+      toggler.innerHTML = "(condense)";
+    } else {
+      bt.className = 'condensed';
+      toggler.innerHTML = "(expand)";
+    }
+  }
+  //-->
