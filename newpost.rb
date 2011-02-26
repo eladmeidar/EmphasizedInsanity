764e528bffaf894175203ff87074e9d6c7804f77
eladmeidar <elad@eizesus.com>
2011-02-27 00:00:41 +0200

    New blog post

--- newpost.rb
+++ newpost.rb
@@ -2,15 +2,15 @@ puts "generating new bag o' links"
 
 current_date = Time.now
 
-title = ARGV[0]
+title = gets.chomp
 year = current_date.year
 month = current_date.month
 day = current_date.day
 
-filename = "_posts/#{year}-#{month}-#{day}-#{title}-#{day}-#{month}-#{year}.textile"
+filename = "_posts/#{year}-#{month}-#{day}-#{title.gsub(" ", '_')}.textile"
 content=<<-EOF
 --- 
-title:
+title: #{title}
 layout: post
 ---
 EOF
